6a79a21bc873fee017be39a65c3b95c824920ea1
Jxyden_ <125519351+JayDevBot@users.noreply.github.com>
2025-03-19 12:37:30 +0100

    Removed users

--- ProjectDatabases/obj/Debug/net8.0/ProjectDatabases.AssemblyInfo.cs
+++ ProjectDatabases/obj/Debug/net8.0/ProjectDatabases.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjectDatabases")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f14608b2f2b8b0e16b70dbddf6a17908bb4e6061")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c79a9b4753021421c119641f1c9d744dede2d8bb")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjectDatabases")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjectDatabases")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
